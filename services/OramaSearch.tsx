2ca9396a3f3a2edccabc934dd75afdfe339cef18
Ace G <aceguevarra48@gmail.com>
2024-11-09 11:12:44 +0800

    refactor: Styling and User LocalStorage persistence

--- services/OramaSearch.tsx
+++ services/OramaSearch.tsx
@@ -22,9 +22,10 @@ import { CardDescription } from "@/components/ui/card";
 import { AnimatePresence, motion } from "framer-motion";
 import { priceFormatter } from "@/lib/utils";
 import { useDebounceCallback } from "usehooks-ts";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function OramaSearch() {
-  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [isClicked, setIsClicked] = useState<boolean>();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [search, setSearch] = useState<string>("");
@@ -102,101 +103,113 @@ export function OramaSearch() {
           </kbd>
         </Button>
       </DialogTrigger>
-      <DialogContent className="!rounded-xl px-4 h-96 overflow-auto">
-        <DialogHeader className="pt-6 space-y-4">
-          <DialogTitle className="text-center -tracking-tighter uppercase font-extrabold text-[#FBA328]">
-            Ask me anything
-          </DialogTitle>
+      <DialogContent className="!rounded-xl px-4">
+        <ScrollArea className="mt-4 h-96">
+          <div className="pl-2 pb-4 pr-4 space-y-6 md:space-y-8">
+            <DialogHeader className="pt-6 space-y-4">
+              <DialogTitle className="text-center -tracking-tighter uppercase font-extrabold text-[#FBA328]">
+                Ask me anything
+              </DialogTitle>
 
-          <Input
-            autoFocus
-            placeholder="Search for products..."
-            className="text-xs md:text-sm"
-            value={search}
-            onChange={async (e) => {
-              if (e.target.value == null) return;
-              setSearch(e.target.value);
-              await debouncedOramaSearch(e.target.value);
-            }}
-          />
-        </DialogHeader>
+              <Input
+                autoFocus
+                placeholder="Search for products..."
+                className="text-xs md:text-sm"
+                value={search}
+                onChange={async (e) => {
+                  if (e.target.value == null) return;
+                  setSearch(e.target.value);
+                  await debouncedOramaSearch(e.target.value);
+                }}
+              />
+            </DialogHeader>
 
-        <AnimatePresence>
-          {isLoading && (
-            <motion.div
-              initial={{
-                opacity: 0,
-                display: "none",
-              }}
-              animate={{
-                opacity: 1,
-                display: "block",
-              }}
-              exit={{
-                opacity: 0,
-                display: "none",
-              }}
-              className="mx-auto w-max"
-            >
-              <Loader className="animate-spin stroke-muted-foreground" />
-            </motion.div>
-          )}
-        </AnimatePresence>
-
-        {results != null && results.length > 0 && (
-          <>
-            <div className="mt-0 md:mt-4 w-full flex items-center justify-between">
-              <CardDescription className="mt-2 text-xs font-medium -tracking-tighter">
-                {results.length} Results
-              </CardDescription>
-              <Button
-                disabled={results.length <= 1}
-                onClick={() => setIsClicked((prev) => !prev)}
-                variant={"ghost"}
-                size={"sm"}
-                className="gap-2"
-              >
-                Price
-                <ArrowUpDown />
-              </Button>
-            </div>
-            <div className="w-full space-y-2">
-              {results.map((hit: Result<Product>) => (
+            <AnimatePresence>
+              {isLoading && (
                 <motion.div
-                  layout
-                  transition={{
-                    type: "spring",
-                    damping: 25,
-                    stiffness: 120,
+                  initial={{
+                    opacity: 0,
+                    display: "none",
                   }}
-                  className="w-full flex gap-4 rounded-md p-4 border hover:shadow-md transition-shadow"
-                  key={hit.id}
+                  animate={{
+                    opacity: 1,
+                    display: "block",
+                  }}
+                  exit={{
+                    opacity: 0,
+                    display: "none",
+                  }}
+                  className="mx-auto w-max"
                 >
-                  <Image
-                    src={hit.document.image}
-                    alt={hit.document.description}
-                    width={50}
-                    height={50}
-                    className="object-contain"
-                  />
-                  <div className="flex flex-col gap-4 w-full">
-                    <p className="text-sm font-medium -tracking-tighter text-pretty">
-                      {hit.document.title}
-                    </p>
-                    <div className="w-full flex items-center justify-between">
-                      <p className="text-sm">
-                        {priceFormatter(hit.document.price)}
-                      </p>
-                      <div onClick={() => setIsOpen(false)}>
-                        <LearnMore variant="link" id={hit.document.id} />
-                      </div>
-                    </div>
-                  </div>
+                  <Loader className="animate-spin stroke-muted-foreground" />
                 </motion.div>
-              ))}
-            </div>
-          </>
-        )}
+              )}
+            </AnimatePresence>
+
+            {results != null && results.length > 0 && (
+              <>
+                <div className="mt-0 md:mt-4 w-full flex items-center justify-between">
+                  <CardDescription className="mt-2 text-xs font-medium -tracking-tighter">
+                    {results.length} Results
+                  </CardDescription>
+                  <Button
+                    disabled={results.length <= 1}
+                    onClick={() =>
+                      setIsClicked((prev) => {
+                        if (prev == null) {
+                          return true;
+                        } else {
+                          return !prev;
+                        }
+                      })
+                    }
+                    variant={"ghost"}
+                    size={"sm"}
+                    className="gap-2"
+                  >
+                    Price
+                    <ArrowUpDown />
+                  </Button>
+                </div>
+                <div className="w-full space-y-2">
+                  {results.map((hit: Result<Product>) => (
+                    <motion.div
+                      layout
+                      transition={{
+                        type: "spring",
+                        damping: 25,
+                        stiffness: 120,
+                      }}
+                      className="w-full flex gap-4 rounded-md p-4 border hover:shadow-md transition-shadow"
+                      key={hit.id}
+                    >
+                      <Image
+                        src={hit.document.image}
+                        alt={hit.document.description}
+                        width={50}
+                        height={50}
+                        className="object-contain"
+                      />
+                      <div className="flex flex-col gap-4 w-full">
+                        <p className="text-sm font-medium -tracking-tighter text-pretty">
+                          {hit.document.title}
+                        </p>
+                        <div className="w-full flex items-center justify-between">
+                          <p className="text-sm">
+                            {priceFormatter(hit.document.price)}
+                          </p>
+                          <div onClick={() => setIsOpen(false)}>
+                            <LearnMore variant="link" id={hit.document.id} />
+                          </div>
+                        </div>
+                      </div>
+                    </motion.div>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
